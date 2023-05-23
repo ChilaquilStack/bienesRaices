54835ec8a6d9c32d6007328adac9c8f218e36ec8
Eddie <Edgar_Villegas_B@outlook.com>
2023-05-23 14:16:31 -0600

    Habilitando el ESModules en Node

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("express");
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.json({ msg: "Hola Mundo en express" });
+})
+
+app.get("/nosotros", (req, res)=> {
+    res.send("Informacion de nosotros")
+})
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor listen port ${port}`);
+});
