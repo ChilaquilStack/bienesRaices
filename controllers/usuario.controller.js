dc8795160f8f46d72c36af77dfe5922fc50d616f
Eddie <Edgar_Villegas_B@outlook.com>
2023-05-26 18:12:44 -0600

    Validar que el usaurio aun no exista

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -10,23 +10,47 @@ const registro = ( req, res ) => res.render('auth/registro', {
 })
 
 const registrar = async ( req, res ) => {
+
     await check('nombre').notEmpty().withMessage("El nombre no puede estar vacio").run(req)
     await check('email').isEmail().withMessage("El email no es valido").run(req)
-    await check('password').isLength({min:6}).withMessage("El password debe ser de minimo 6 caracteres").run(req)
-    await check('repetir_password').equals('password').withMessage("Los password no coinciden").run(req)
+    await check('password').isLength({ min:6 }).withMessage("El password debe ser de minimo 6 caracteres").run(req)
+    await check('repetir_password').equals(req.body.password).withMessage("Los passwords no son iguales").run(req)
 
     const resultado = validationResult(req)
+
+
+    const { nombre, email } = req.body
     
-    if(!resultado.isEmpty()){
+    if(!resultado.isEmpty()) {
         return res.render('auth/registro', {
             titulo: 'Crear cuenta',
-            errores: resultado.array()
+            errores: resultado.array(),
+            usuario: {
+                nombre,
+                email,
+            }
         })
     }
-    
+
+    const existeUsuario = await Usuario.findOne({ where: { email }})
+
+    if(existeUsuario) {
+        return res.render('auth/registro', {
+            titulo: 'Crear cuenta',
+            errores: [{ msg: "El usuario ya existe" }],
+            usuario: {
+                nombre,
+                email,
+            }
+        })
+    }
+
     try {
         const usuario = await Usuario.create(req.body)
-        console.log("Usuario creado")
+        return res.render('auth/registro', {
+            titulo: 'Crear cuenta',
+            usuario
+        })
     } catch (error) {
         console.error("Usuario no creado: ", error)
     }
